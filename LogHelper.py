24b4fa65c1ae83897dbd39ca5939c74d9997a02c
withdrawmemory <withdrawmemory@outlook.com>
2023-07-16 21:06:18 +0800

    测试

--- LogHelper.py
+++ LogHelper.py
@@ -1,11 +1,10 @@
 import time
 
-class LogHelper:
-    def SaveLog(saveName):
-        print("save path is", saveName)
-        #写入文件，二进制的方式
-        file = open(saveName, 'wb')
-        file.write("测试\n".encode("UTF-8"))
-        localtime = "当前时间：" + str(time.localtime(time.time()))
-        file.write(localtime.encode("UTF-8"))
-        file.close()
+def LogHelper.SaveLog(saveName):
+    print("save path is", saveName)
+    #写入文件，二进制的方式
+    file = open(saveName, 'wb')
+    file.write("测试\n".encode("UTF-8"))
+    localtime = "当前时间：" + str(time.localtime(time.time()))
+    file.write(localtime.encode("UTF-8"))
+    file.close()
